e49871cbfe5ef3f4ca7dda6bae9da5911dff5ed6
Hieu So <hieuphso@gmail.com>
2023-10-27 21:41:40 -0400

    adding menu into page

--- src/main/java/com/virtualpairprogrammers/servlets/HelloWorldServlet.java
+++ src/main/java/com/virtualpairprogrammers/servlets/HelloWorldServlet.java
@@ -2,15 +2,11 @@ package com.virtualpairprogrammers.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
-import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.virtualpairprogrammers.data.MenuDataService;
-import com.virtualpairprogrammers.domain.MenuItem;
 
 public class HelloWorldServlet extends HttpServlet {
 
@@ -18,10 +14,9 @@ public class HelloWorldServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		response.setContentType("text/html");
 		
-		MenuDataService menuDataService = new MenuDataService();
-		List<MenuItem> menuItems = menuDataService.getFullMenu();
+		
 		out.println("<html><body><h1>Hello World</h1>");
-		out.println("<p>The time is : " + new Date() + "</p>");
+		
 		out.println("</body></html>");
 		out.close();
 	}
